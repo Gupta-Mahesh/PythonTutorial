60be495ddf80d7954284ef768f03365e88568239
Mahesh Gupta <maheshkumargupta1792@gmail.com>
2023-07-12 05:54:11 +0530

    Class level access | private and suggested private

--- Classes/ClassLevelAccess.py
+++ Classes/ClassLevelAccess.py
@@ -8,5 +8,15 @@ class Car:
         self._fuel="Petrol"             #Sinle underscore | Suggest that Don't use this variable, it might be change | like private
         self.__color="White"            #Double underscore | It is not acceesable outside the class directly | but can be used |private variable
 
+car=Car()
 
+#accessing the variables
+print(car.model)
+
+#accessing the suggested to avoid var
+print(car._fuel)
+
+#accessing the private member
+print(car.__color)                  #It will throw error
+                    # But it can be acceessble using --> print(car._Car__color)
 
